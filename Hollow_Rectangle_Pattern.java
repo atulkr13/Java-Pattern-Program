f7cc713a23addedb22c58f03c281fe82a66915c1
Atul kumar <126343803+atulkr13@users.noreply.github.com>
2025-11-07 23:29:04 +0530

    Add files via upload

---
+++ Hollow_Rectangle_Pattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Hollow_Rectangle_Pattern
+{
+public static void main(String[] args)
+{
+Scanner scn = new Scanner(System.in);
+System.out.println("Enter the number");
+int n = scn.nextInt();
+for(int i = 1;i<=n;i++)
+{
+for(int j = 1;j <= n;j++)
+{
+if(i == n || i == 1 || j == n || j == 1)
+{
+System.out.print("X");
+}
+else
+{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}
