498ff00d2c29e3658ef8d2918b8bba8ac90cc677
Atul kumar <126343803+atulkr13@users.noreply.github.com>
2025-11-07 23:32:12 +0530

    Add files via upload

---
+++ hollow_diamond.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class hollow_diamond
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = scn.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++)
+            {
+                if (j == 1 || j == (2 * i - 1))
+                    System.out.print("X");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++)
+            {
+                if (j == 1 || j == (2 * i - 1))
+                    System.out.print("X");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
