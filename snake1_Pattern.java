e54b97eb8e62a171984f1f163cc3eabbfe05db3f
Atul kumar <126343803+atulkr13@users.noreply.github.com>
2025-11-07 23:29:46 +0530

    Add files via upload

---
+++ snake1_Pattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class snake1_Pattern
+{
+public static void main(String[] args)
+{
+Scanner scn = new Scanner(System.in);
+System.out.println("Enter the number");
+int n = scn.nextInt();
+for(int i = 1;i <= n;i++)
+{
+if(i % 2 != 0)
+{
+for(int j = 1;j<=n;j++)
+{
+System.out.print(j+" ");
+}
+}
+else
+{
+for(int j = n;j >= 1;j--)
+{
+System.out.print(j + " ");
+}
+}
+System.out.println();
+}
+}
+}
